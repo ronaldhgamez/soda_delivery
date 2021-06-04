a990b149ac027b038e4b6ea23bc886cacdfeb962
AliciaD2018 <aliciadiazrivas@gmail.com>
2021-06-03 23:36:42 -0600

    update

--- src/Views/ModifySoda.js
+++ src/Views/ModifySoda.js
@@ -1,103 +1,83 @@
-import React, { Component } from 'react';
-import { View,Picker } from 'react-native';
-import style from '../Styles/ModifyPS_Style';
-import {Text,Icon,Button,Input} from 'react-native-elements';
+import React, { Component, useState } from 'react';
+import { View,Picker,TextInput } from 'react-native';
+import style from '../Styles/ModifySoda_Style';
+import {Text,Icon,Button} from 'react-native-elements';
 
 
 class ModifySoda extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      nombre:"",
+      dueno:"",
+      descrip:"",
+      direccion:"",
+      tipoComida:"",
+      contrasena:"",
+      usuario:"",
       spinner: false,
       value: 0,
       PickerSelectedVal: ''
     };
   }
 
+  //consultar el nombre de soda y si es igual hacer la modificacion o eliminar
+  solicitud = () => {
+      
+  }
+
   render() {
     return (
-      
       <View style={style.container}>
+        <Text></Text>
         <Text style = {style.logo}> Modificar Soda</Text>
-          <Input
-          placeholder='Nombre - soda'
-          leftIcon={
-          <Icon
-           name='edit'
-           type="font-awesome"
-           size={24}
-           color='black'
-         />
-         } 
+
+        <Icon name='edit'type="font-awesome"size={15} color='black'/>
+          <TextInput placeholder='Nombre - soda'
+          style={style.input}
+          onChangeText={value => this.setState({ comment: value })}  
         />
-        <Input
+
+        <Icon name='edit'type="font-awesome"size={15} color='black'/>
+        <TextInput
           placeholder='Propietario'
-          leftIcon={
-          <Icon
-           name='edit'
-           type= 'font-awesome'
-           size={24}
-           color='black'
-         />
-         } 
+          style={style.input}
+          onChangeText={value => this.setState({ comment: value })} 
         />
 
-        <Input
+        <Icon name='edit'type="font-awesome"size={15} color='black'/>
+        <TextInput
           placeholder='Descripcion'
-          leftIcon={
-          <Icon
-           name='edit'
-           type= 'font-awesome'
-           size={24}
-           color='black'
-         />
-         } 
-        />
-        <Input
-          placeholder='Direccion exacta'
-          leftIcon={
-          <Icon
-           name='edit'
-           type= 'font-awesome'
-           size={24}
-           color='black'
-         />
-         } 
-        />
-        <Input
-          placeholder='Tipo comida'
-          leftIcon={
-          <Icon
-           name='edit'
-           type= 'font-awesome'
-           size={24}
-           color='black'
-         />
-         } 
-        />
-        <Input
-          placeholder='Contraseña'
-          leftIcon={
-          <Icon
-           name='edit'
-           type= 'font-awesome'
-           size={24}
-           color='black'
-         />
-         } 
+          style={style.input}
+          onChangeText={value => this.setState({ comment: value })} 
         />
 
-        <Input
+        <Icon name='edit'type="font-awesome"size={15} color='black'/> 
+        <TextInput
+          placeholder='Direccion exacta' 
+          style={style.input}
+          onChangeText={value => this.setState({ comment: value })} 
+        />
+
+        <Icon name='edit'type="font-awesome"size={15} color='black'/>
+        <TextInput
+          placeholder='Tipo comida'
+          style={style.input} 
+          onChangeText={value => this.setState({ comment: value })} 
+        />
+
+        <Icon name='edit'type="font-awesome"size={15} color='black'/>
+        <TextInput
+          placeholder='Contraseña'
+          style={style.input}
+          onChangeText={value => this.setState({ comment: value })} 
+        />
+
+        <Icon name='edit'type="font-awesome"size={15} color='black'/>
+        <TextInput
           placeholder='usuario'
-          
-          leftIcon={
-          <Icon
-           name='edit'
-           type= 'font-awesome'
-           size={24}
-           color='black'
-         />
-         } 
+          style={style.input}
+          onChangeText={value => this.setState({ comment: value })} 
         />
     
 
