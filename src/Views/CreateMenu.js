dc5c6ada1f731437cf4b01659f11b731fc3c0bf5
retanaalyson <79118676+retanaalyson@users.noreply.github.com>
2021-06-09 21:17:07 -0600

    Update CreateMenu.js

--- src/Views/CreateMenu.js
+++ src/Views/CreateMenu.js
@@ -1,78 +1,77 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, TextInput, Image, Button } from 'react-native'
+import {View, Text, TouchableOpacity, TextInput, Image, Button} from 'react-native'
 import style from '../Styles/CreateMenu_Style'
 
-import { addProduct } from '../Utilities/CreateMenuCon'
+import { addProduct} from '../Utilities/CreateMenuCon'
 
 export default class CreateMenu extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            product_fields: []
+            product_fields:[],
+            idMenu : this.props.route.params.idMenu
         }
     }
 
     save = async () => {
-        console.log(this.props.route.params.id + "******************************");
-        //await this.state.product_fields.map(n => addProduct(this.props.route.params.id,  n.meta_name, n.meta_price))
+        await this.state.product_fields.map(n => addProduct({idMenu: "ccnySGMUiQPDEQlGBbAJ", name: n.meta_name, price: n.meta_price}));
     }
 
-    addProductField = () => {
+    addProductField = () =>{
         this.setState({
-            product_fields: [...this.state.product_fields, { meta_name: 'value', meta_price: 'value' }]
+            product_fields:[...this.state.product_fields,{meta_name:'value', meta_price:'value'}]
         })
     }
-    OnProductInputNameHandler = (value, index) => {
+    OnProductInputNameHandler = (value,index) => {
         this.state.product_fields[index].meta_name = value;
-        this.setState({ product_fields: this.state.product_fields });
+        this.setState({product_fields:this.state.product_fields});
     }
-    OnProductInputPriceHandler = (value, index) => {
+    OnProductInputPriceHandler= (value,index) =>{
         this.state.product_fields[index].meta_price = value;
-        this.setState({ product_fields: this.state.product_fields });
+        this.setState({product_fields:this.state.product_fields});
     }
-    deleteDynamicField = (index) => {
-        this.state.product_fields.splice(index, 1);
-        this.setState({ product_fields: this.state.product_fields });
+    deleteDynamicField = (index) =>{
+        this.state.product_fields.splice(index,1);
+        this.setState({product_fields:this.state.product_fields});
     }
+
     render() {
         return (
             <View style={style.container}>
-                <Text style={style.lblTittle}> Menú </Text>
+                <Text style = {style.lblTittle}> Menú </Text>
+
                 {
-                    this.state.product_fields.map((productInput, k) => {
-                        {
-                            return (
-                                <View style={style.inputsContainer}>
-                                    <View style={[style.inputContainer]}>
-                                        <TextInput
-                                            style={style.input}
-                                            onChange={name => { this.OnProductInputNameHandler(name, k) }}
-                                            placeholder={'Nombre del Producto'} />
-                                    </View>
-                                    <View style={[style.inputContainer]}>
-                                        <TextInput
-                                            style={style.input}
-                                            onChange={price => { this.OnProductInputPriceHandler(price, k) }}
-                                            placeholder={'Precio'} />
-                                    </View>
-                                    <TouchableOpacity onPress={() => this.deleteDynamicField(k)} >
-                                        <Text style={style.addBtnText}>
-                                            {<Image
-                                                style={style.iconHistorial}
-                                                source={require('../../assets/eliminar.png')}
-                                            />}
-                                        </Text>
-                                    </TouchableOpacity>
+                    this.state.product_fields.map((productInput,k) =>{
+                        return(
+                            <View key = {k} style = {style.inputsContainer}>
+                                <View style = {[style.inputContainer]}>
+                                    <TextInput
+                                        style = {style.input}
+                                        value = {productInput.key}
+                                        onChangeText={name =>{this.OnProductInputNameHandler(name,k)}}
+                                        placeholder={'Nombre del Producto'}/>
                                 </View>
-                            )
-                        }
+                                <View style = {[style.inputContainer]}>
+                                    <TextInput
+                                        style={style.input}
+                                        onChangeText={price => {this.OnProductInputPriceHandler(price,k)}}
+                                        placeholder={'Precio'}/>
+                                </View>
+                                <TouchableOpacity onPress = {() => this.deleteDynamicField(k)} >
+                                    <Text style = {[style.addBtnText]}>
+                                        {<Image
+                                            style={style.iconHistorial}
+                                            source={require('../../assets/eliminar.png')}/>}</Text>
+                                </TouchableOpacity>
+                            </View>
+
+                        )
                     })
                 }
-
                 <TouchableOpacity
                     style={style.addBtn}
-                    onPress={() => { this.addProductField() }}>
-                    <Text style={style.addBtnText}> Agregar Producto</Text>
+                    onPress={()=>{this.addProductField()}}>
+                    <Text style = {style.addBtnText}> Agregar Producto</Text>
                 </TouchableOpacity>
 
                 <Button
